11d035fea991e99d2fa05c7a6a7b0933857ddd1b
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-25 06:06:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,14 +2,13 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Montague"
-        age: "53"
+        name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague", age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
